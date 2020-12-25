334e225a10afc23cc5c7a5f6724cfd0f0b953a2f
Guo Jiusen <2434732570@qq.com>
2020-12-25 14:34:06 +0800

    springboot版本项目搭建

---
+++ scbo2o/src/main/java/com/imooc/scbo2o/Scbo2oApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.scbo2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scbo2oApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scbo2oApplication.class, args);
+    }
+
+}
---
+++ scbo2o/src/test/java/com/imooc/scbo2o/Scbo2oApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.scbo2o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scbo2oApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
